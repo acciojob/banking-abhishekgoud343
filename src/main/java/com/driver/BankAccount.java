5a5bf3af1b7f33ad562daf60a46a23f50d974480
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-09-20 01:44:07 +0530

    Test mod4

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -16,39 +16,41 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        StringBuilder accountNo = new StringBuilder();
         int rem = sum;
+        String accountNo = "";
 
-        if(digits * 9 < rem)
+        if(digits * 9 < sum)
             throw new Exception("Account Number can not be generated");
-
         else {
-            while (digits-- > 0 && rem > 0) {
+            while (digits > 0 && rem > 0) {
                 if (rem > 9) {
-                    accountNo.append("9");
+                    accountNo = accountNo + "9";
                     rem -= 9;
                 } else {
-                    accountNo.append(Integer.toString(rem));
+                    accountNo = accountNo + Integer.toString(rem);
                     rem = 0;
                 }
+                --digits;
             }
 
-            while (digits-- > 0)
-                accountNo.append("0");
+            while (digits > 0) {
+                accountNo = accountNo + "0";
+                --digits;
+            }
 
-            return accountNo.toString();
+            return accountNo;
         }
     }
 
     public void deposit(double amount) {
         //add amount to balance
-        balance += amount;
+        balance = balance + amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
         if (balance - amount >= minBalance)
-            balance -= amount;
+            balance = balance - amount;
         else
             throw new Exception("Insufficient Balance");
     }
