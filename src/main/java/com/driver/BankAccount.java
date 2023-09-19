c789289b3a502475af05e652a6451aadcf2b0f00
Abhishek Akkapalli <abhishekgoud.jobs@gmail.com>
2023-09-20 02:15:43 +0530

    test 12

--- src/main/java/com/driver/BankAccount.java
+++ src/main/java/com/driver/BankAccount.java
@@ -16,40 +16,42 @@ public class BankAccount {
         //Each digit of an account number can lie between 0 and 9 (both inclusive)
         //Generate account number having given number of 'digits' such that the sum of digits is equal to 'sum'
         //If it is not possible, throw "Account Number can not be generated" exception
-        if(digits * 9 < sum)
-            throw new Exception("Account Number can not be generated");
-
         StringBuilder accountNo = new StringBuilder();
-
         int j = (int) Math.ceil((double) sum / digits);
-        int i = j;
-        while (digits > 0 && sum >= 0) {
-            if (i > 9)
-                i = j;
-            if (sum < i)
-                i = sum;
 
-            accountNo.append(i);
-            sum -= i;
+        if (j <= 9) {
+            int i = j;
+            while (digits > 0 && sum >= 0) {
+                if (i > 9)
+                    i = j;
+                if (sum < i)
+                    i = sum;
 
-            ++i;
-            --digits;
+                accountNo.append(i);
+                sum -= i;
+
+                ++i;
+                --digits;
+            }
+
+            return accountNo.toString();
         }
-
-        return accountNo.toString();
+        else
+            throw new Exception("Account Number can not be generated");
     }
 
     public void deposit(double amount) {
         //add amount to balance
-        balance += amount;
+        if (amount > 0)
+            balance += amount;
     }
 
     public void withdraw(double amount) throws Exception {
         // Remember to throw "Insufficient Balance" exception, if the remaining amount would be less than minimum balance
-        if (balance - amount < minBalance)
+        if (balance - amount >= minBalance)
+            balance -= amount;
+        else
             throw new Exception("Insufficient Balance");
-
-        balance -= amount;
     }
 
     public String getName() {
